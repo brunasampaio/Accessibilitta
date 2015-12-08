4dc4388adec7cccb6bf9341ff798bf86dd6d6804
Rafael Silva <rafuxto06@gmail.com>
2015-12-08 04:20:37 -0200

    Incluindo projeto com app

---
+++ Accessibilita/Accessibilitta.App/www/js/controllers.js
@@ -0,0 +1,22 @@
+angular.module('app.controllers', [])
+  
+.controller('registrarCtrl', function($scope) {
+
+})
+   
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('buscarLocalCtrl', function($scope) {
+
+})
+      
+.controller('perfilCtrl', function($scope) {
+
+})
+   
+.controller('avaliacaoCtrl', function($scope) {
+
+})
+ 
